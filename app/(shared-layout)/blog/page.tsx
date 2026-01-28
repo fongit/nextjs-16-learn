b2b54c4febfcb43cad9928283faaa3ef85d5438c
fong <botlorkfong@gmail.com>
2026-01-28 18:12:01 +0700

    added use cache

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -4,10 +4,10 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { api } from '@/convex/_generated/api';
 import { fetchQuery } from 'convex/nextjs';
 import { Metadata } from 'next';
-// import { cacheLife, cacheTag } from 'next/cache';
+import { cacheLife, cacheTag } from 'next/cache';
 import Image from 'next/image';
 import Link from 'next/link';
-import { connection } from 'next/server';
+// import { connection } from 'next/server';
 import { Suspense } from 'react';
 
 // layout.tsx | pate.tsx | route.tsx
@@ -43,10 +43,9 @@ export default function BlogPage() {
 }
 
 async function LoadBlogList() {
-	// 'use cache';
-	// cacheLife('hours');
-	// cacheTag('blog');
-	await connection();
+	'use cache';
+	cacheLife('hours');
+	cacheTag('blog');
 	const data = await fetchQuery(api.posts.getPosts);
 	return (
 		<div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
