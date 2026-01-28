a373950b6e8039c19a0ec3ead9302d19e37c388b
fong <botlorkfong@gmail.com>
2026-01-28 17:44:31 +0700

    small fix

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { api } from '@/convex/_generated/api';
 import { fetchQuery } from 'convex/nextjs';
 import { Metadata } from 'next';
-import { cacheLife, cacheTag } from 'next/cache';
+// import { cacheLife, cacheTag } from 'next/cache';
 import Image from 'next/image';
 import Link from 'next/link';
 import { connection } from 'next/server';
@@ -43,9 +43,10 @@ export default function BlogPage() {
 }
 
 async function LoadBlogList() {
-	'use cache';
-	cacheLife('hours');
-	cacheTag('blog');
+	// 'use cache';
+	// cacheLife('hours');
+	// cacheTag('blog');
+	await connection();
 	const data = await fetchQuery(api.posts.getPosts);
 	return (
 		<div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
